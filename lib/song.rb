ea521758f4ea578fb22b65018a450043cb743004
Karl Benbeneck <kbenbeneck@gmail.com>
2019-10-22 00:37:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,35 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    song.save
+    return song
+  end
+
+  def self.new_by_name
+
+  end
+
+  def self.create_by_name
+  end
+
+  def find_by_name
+  end
+
+  def find_or_create_by_name
+  end
+
+  def alphabetical
+  end
+
+  def new_by_name
+  end
+
+  def create_from_filename
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
 end
