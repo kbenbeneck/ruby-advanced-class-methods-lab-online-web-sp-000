817270c16a6d55ceaff164866acf191d00cc0080
Karl Benbeneck <kbenbeneck@gmail.com>
2019-10-22 01:01:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -55,6 +55,7 @@ class Song
     parts = filename.split(" - ")
     artist_name = parts[0]
     song_name = parts[1].gsub(".mp3", "")
+
     song = self.create
     song.name = song_name
     song.artist_name = artist_name
