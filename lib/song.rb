697f03472864e19c62f82fea7b6317689e66b461
Karl Benbeneck <kbenbeneck@gmail.com>
2019-10-22 00:59:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,7 +40,7 @@ class Song
     self.all.sort_by{|s| s.name}
   end
 
-  def create_from_filename
+  def create_from_filename(filename)
     parts = filename.split(" - ")
     artist_name = parts[0]
     song_name = parts[1].gsub(".mp3", "")
