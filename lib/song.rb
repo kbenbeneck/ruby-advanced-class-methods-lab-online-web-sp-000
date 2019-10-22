a616fd0e729aae3a4a60fd2b2286e174b82d7002
Karl Benbeneck <kbenbeneck@gmail.com>
2019-10-22 00:55:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,7 +32,8 @@ class Song
     self.all.detect{|s| s.name == song_name}
   end
 
-  def find_or_create_by_name
+  def self.find_or_create_by_name(song_name)
+    self.find_by_name(song_name)||self.create_by_name(song_name)
   end
 
   def alphabetical
