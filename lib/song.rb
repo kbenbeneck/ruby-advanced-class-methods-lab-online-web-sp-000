680ae8e00de24dc5825081a8b1a3084a68a34dd8
Karl Benbeneck <kbenbeneck@gmail.com>
2019-10-22 00:51:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,13 +16,16 @@ class Song
     song
   end
 
-  def self.new_by_name (song_name)
+  def self.new_by_name(song_name)
     song = self.new
     song.name = song_name
     song
   end
 
-  def self.create_by_name
+  def self.create_by_name(song_name)
+    song = self.create
+    song.name = song_name
+    song
   end
 
   def find_by_name
